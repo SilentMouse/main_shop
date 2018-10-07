6e5e2bdfbe2889f80433c0b13e239ea2a8bc9c8d
maxim <mymailforxperia@gmail.com>
2018-10-07 14:51:54 +0300

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+puts "*"*5 + " PRODUCTION " + "*"*5
+server "fornow", user: "rbdev", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -17,9 +17,12 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{fornow}, my_property: :my_value
+role :web, %w{fornow}, other_property: :other_value
+role :db,  %w{fornow}
+
+
+set :deploy_to, '/home/rbdev/fornow'
 
 
 
