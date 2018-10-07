185e24ef4ddc7b5008a7482f65301761c0ffc174
maxim <mymailforxperia@gmail.com>
2018-10-07 15:03:54 +0300

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "main_shop"
-set :repo_url, "git@github.com:Solver-Club/main_shop.git"
+set :repo_url, "git@github.com:SilentMouse/main_shop.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
