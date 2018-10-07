fc154724e172579fd2b3d3f0aea8056627f1fe34
maxim <mymailforxperia@gmail.com>
2018-10-07 14:50:18 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,17 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "main_shop"
+set :repo_url, "git@github.com:Solver-Club/main_shop.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/home/rbdev/main_shop'
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('config/thin','bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system','public/uploads')
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
