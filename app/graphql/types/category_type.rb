3e864361d8cc2ebc1e65a6274dcf284214eef219
maxim <mymailforxperia@gmail.com>
2018-11-05 12:53:58 +0300

    new real

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -1,5 +1,6 @@
 module Types
   class CategoryType < Types::BaseObject
+    field :id, Integer, null: true
     field :name, String, null: true
     field :products, [Types::ProductType], null: true
   end
